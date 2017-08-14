fe85006587bef77d6581904b4799c08d0540b809
JTLANDER97 <jordan.holmes@my.jcu.edu.au>
2017-08-14 14:31:08 +1000

    Add copies of broken_score and temperatures to practical 3 folder

--- prac_03/odd_name.py
+++ prac_03/odd_name.py
@@ -1,14 +1,27 @@
 """Jordan Holmes"""
 
-valid_user_name = False
-user_name = str
 
-while valid_user_name is not True:
+def main():
+    valid_user_name = False
+    user_name = str()
+    while valid_user_name is not True:
+        user_name, valid_user_name = get_name(user_name, valid_user_name)
+    frequency_of_letters = int(input("How frequently do you want to skip letters?: "))
+    prints_the_name(user_name, frequency_of_letters)
+
+
+def prints_the_name(user_name, frequency_of_letters):
+    for char in range(0, (len(user_name) + 1), frequency_of_letters):
+        print(user_name[char])
+
+
+def get_name(user_name, valid_user_name):
     user_name = str(input("What is your name?: "))
     if user_name == "":
         print("Invalid entry!")
     else:
         valid_user_name = True
+    return user_name, valid_user_name
 
-for char in range(0, (len(user_name) + 1), 2):
-    print(user_name[char])
+
+main()
