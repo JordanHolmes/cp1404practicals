8d230559c550ec866caa2da23bd29e9cccb6fe95
JordanHolmes <jordan.holmes@my.jcu.edu.au>
2017-08-23 14:19:05 +1000

    Changes advised by tutor

--- prac_04/quick_pick_generator.py
+++ prac_04/quick_pick_generator.py
@@ -1,25 +1,25 @@
 import random
 
+AMOUNT_OF_NUMBERS_IN_QUICK_PICK = 6
+
+MAX_VALUE = 45
+
 
 def main():
     number_of_quick_picks = int(input("How many quick picks? "))
     for quick_pick in range(number_of_quick_picks):
         quick_pick = []
-        for i in range(0, 6):
-            new_number = random.randint(1, 45)
-            quick_pick.append(check_if_unique_number(new_number, quick_pick))
+        for i in range(AMOUNT_OF_NUMBERS_IN_QUICK_PICK):
+            new_number = random.randint(1, MAX_VALUE)
+            quick_pick.append(generate_unique_number(new_number, quick_pick))
             quick_pick.sort()
-        print("{:2} {:2} {:2} {:2} {:2} {:2}".format(quick_pick[0], quick_pick[1], quick_pick[2], quick_pick[3],
-                                                     quick_pick[4],
-                                                     quick_pick[5]))
+        print("{:2} {:2} {:2} {:2} {:2} {:2}".format(*quick_pick))
 
 
-def check_if_unique_number(new_number, quick_pick):
+def generate_unique_number(new_number, quick_pick):
     while new_number in quick_pick:
-        new_number = random.randint(1, 45)
+        new_number = random.randint(1, MAX_VALUE)
     return new_number
 
 
 main()
-
-# TODO: Change print line to a neater format
