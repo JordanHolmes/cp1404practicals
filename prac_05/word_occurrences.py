aafc243118932c3c2c8383909406f5e067486153
JTLANDER97 <jordan.holmes@my.jcu.edu.au>
2017-08-30 07:48:55 +1000

    Add files for practical 5

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,22 @@
+"""
+CP1404/CP5632 Practical
+Word occurences in a dictionary
+"""
+
+def main():
+    words_to_word_count = {}
+    user_words = input("Text: ").lower().split()
+    for user_word in user_words:
+        if user_word in words_to_word_count:
+            words_to_word_count[user_word] += 1
+        else:
+            words_to_word_count[user_word] = 1
+
+    words = list(words_to_word_count)
+    words.sort()
+    longest_word = max(len(word) for word in words)
+
+    for word in words:
+        print("{:{}} : {:2}".format(word, longest_word, words_to_word_count[word]))
+
+main()
