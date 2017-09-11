52032bd896245603af16d8493fd02eacbd43ef6f
JTLANDER97 <jordan.holmes@my.jcu.edu.au>
2017-09-11 15:13:40 +1000

    Upadte the program & kv file for convert_miles_to_kilometres

--- prac_06/convert_miles_to_kilometres.py
+++ prac_06/convert_miles_to_kilometres.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
 
+MILES_TO_KILOMETRES = 1.60934
 
 class Convertmilestokilometres(App):
     def build(self):
@@ -8,8 +9,20 @@ class Convertmilestokilometres(App):
         self.root = Builder.load_file('convert_miles_to_kilometres.kv')
         return self.root
 
-    def handle_increment(self, value):
-        input_value = self.root.ids.input_name.text
-        self.root.ids.input_name = int(input_value) + value
+    def handle_increment(self, change):
+        try:
+            input_value = float(self.root.ids.input_name.text)
+        except ValueError:
+            input_value = 0
+        self.root.ids.input_name.text = str(input_value + change)
+        self.handle_calculate()
+
+    def handle_calculate(self):
+        try:
+            input_value = float(self.root.ids.input_name.text)
+        except ValueError:
+            input_value = 0
+        self.root.ids.label.text = str(input_value * MILES_TO_KILOMETRES)
+
 
 Convertmilestokilometres().run()
